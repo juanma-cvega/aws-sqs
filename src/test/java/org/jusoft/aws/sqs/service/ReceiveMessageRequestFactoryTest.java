2c4e4a22edd3e55a996bd034f7c76395cfb31b34
Juan Manuel Carnicero Vega <jmcarnicero@ippon.fr>
2018-07-06 17:55:06 +0200

    Fix test using not compatible with maven surefire library.

--- src/test/java/org/jusoft/aws/sqs/service/ReceiveMessageRequestFactoryTest.java
+++ src/test/java/org/jusoft/aws/sqs/service/ReceiveMessageRequestFactoryTest.java
@@ -4,9 +4,9 @@ import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.model.GetQueueUrlResult;
 import com.amazonaws.services.sqs.model.QueueDoesNotExistException;
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
-import org.assertj.swing.security.NoExitSecurityManagerInstaller;
-import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 import org.junit.runner.RunWith;
 import org.jusoft.aws.sqs.QueueConsumer;
 import org.jusoft.aws.sqs.fixture.TestFixtures.SingleParameterMethodClass;
@@ -17,28 +17,20 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.lang.reflect.Method;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.jusoft.aws.sqs.fixture.TestFixtures.QUEUE_URL;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ReceiveMessageRequestFactoryTest {
 
-  private static final String SYSTEM_EXIT_MESSAGE = "System exit";
+  @Rule
+  public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
   @Mock
   private AmazonSQS amazonSQS;
   @InjectMocks
   private ReceiveMessageRequestFactory factory;
 
-  private static NoExitSecurityManagerInstaller noExitSecurityManagerInstaller;
-
-  @BeforeClass
-  public static void setUpOnce() {
-    noExitSecurityManagerInstaller = NoExitSecurityManagerInstaller.installNoExitSecurityManager(i -> {
-      throw new RuntimeException(SYSTEM_EXIT_MESSAGE);
-    });
-  }
 
   @Test
   public void whenCreateFromQueueConsumerThenRequestShouldContainAnnotationConfiguration() throws NoSuchMethodException {
@@ -57,9 +49,8 @@ public class ReceiveMessageRequestFactoryTest {
     QueueConsumer queueConsumer = getQueueConsumer();
     when(amazonSQS.getQueueUrl(queueConsumer.getAnnotation().value())).thenThrow(new QueueDoesNotExistException(""));
 
-    assertThatThrownBy(() -> factory.createFrom(queueConsumer))
-      .isInstanceOf(RuntimeException.class)
-      .hasMessage(SYSTEM_EXIT_MESSAGE);
+    exit.expectSystemExit();
+    factory.createFrom(queueConsumer);
   }
 
   private QueueConsumer getQueueConsumer() throws NoSuchMethodException {
